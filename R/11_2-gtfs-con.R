b62fce93d8fae1a685574772ac3c5a6ee6c509ec
julianatrez <julianatrez@gmail.com>
2022-11-08 13:13:01 +0000

    gtfs contagem

--- R/11_2-gtfs-con.R
+++ R/11_2-gtfs-con.R
@@ -1,5 +1,14 @@
 #GTFS Contagem
 
+install.packages('osmdata')
+install.packages('ggthemes')
+install.packages('purrr')
+library(osmdata)
+library(ggthemes)
+library(mapview)
+library(dplyr)
+library(purrr)
+
 source('R/fun/setup.R')
 
 
@@ -8,15 +17,15 @@ source('R/fun/setup.R')
 # b
 install.packages('sf')
 library(sf)
-stops <- read_sf('../11 - GTFS/muni_con/Base de dados PED.shp')
+stops <- read_sf('../11 - GTFS/muni_con/Base de dados PED_03.shp') %>% st_zm (drop=T)
 
 mapview(stops)
 head(stops)
 
 stops$stop_id <- seq.int(nrow(stops))
-stops2 <- stops %>% select(stop_id) %>% st_transform(crs = "+proj=longlat + datum=WGS84") %>%
+stops2 <- stops %>% select(stop_id, Endere__o) %>% st_transform(crs = "+proj=longlat + datum=WGS84") %>%
   mutate(stop_code = NA,
-         stop_name = Endere_o,
+         stop_name = Endere__o,
          stop_desc = NA,
          stop_lat = unlist(map(.$geometry,2)),
          stop_lon = unlist(map(.$geometry,1)),
@@ -28,13 +37,13 @@ stops2 <- stops %>% select(stop_id) %>% st_transform(crs = "+proj=longlat + datu
          wheelchair_boarding = NA,
          level_id = NA,
          platform_code = NA
-  )
+  ) %>% select (-Endere__o)
 # crs(stops2)
 
 stops2 <- stops2 %>% st_drop_geometry()
 
 write.table(stops2, file = '../11 - GTFS/muni_con/gtfs_files/stops.txt',sep = ',', na = "",
-            row.names = F, quote = F)
+            row.names = F, quote = T)
 
 # Itinerários - shapes.txt ------------------------------------------------
 
