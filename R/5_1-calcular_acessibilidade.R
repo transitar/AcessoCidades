39af8010cf64e64d0455c45ad89dfc48d0662886
Nilso de Brito <nilso.britofilho@gmail.com>
2023-02-16 18:53:27 +0000

    att noh

--- R/5_1-calcular_acessibilidade.R
+++ R/5_1-calcular_acessibilidade.R
@@ -7,7 +7,7 @@ source('./R/fun/setup.R')
 
 
 
-sigla_muni <- "poa"; ano=2022; mode_access = 'transit'; indicator_access <- "active"
+sigla_muni <- "noh"; ano=2022; mode_access = 'transit'; indicator_access <- "active"
 # sigla_muni <- "spo"; ano=2019
 # sigla_muni <- "for"; ano=2019
 # sigla_muni <- "for"; ano=2017
@@ -936,6 +936,8 @@ calcular_acess_muni <- function(sigla_muni, ano, BFCA = FALSE, mode_access = "al
     }
     
   }
+  suppressWarnings(dir.create(sprintf('../r5r/accessibility/muni_%s/acc_%s.rds', 
+                     sigla_muni, sigla_muni), recursive = T))
   
   write_rds(acess_cma, sprintf('../r5r/accessibility/muni_%s/acc_%s.rds', 
                                sigla_muni, sigla_muni))
