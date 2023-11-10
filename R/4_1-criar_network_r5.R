e46a9c6239cd8ab20ffeb4b140b319694558cb9a
Nilso de Brito <nilso.britofilho@gmail.com>
2023-11-10 10:11:33 -0300

    att sigla muni

--- R/4_1-criar_network_r5.R
+++ R/4_1-criar_network_r5.R
@@ -1,16 +1,36 @@
 #criação de graphs no r5r
 rm(list = ls(all.names = T)); gc(full = T)
 
-options(java.parameters = '-Xmx3G')
+options(java.parameters = '-Xmx5G')
 # system("java -version")
 library(r5r)
 library(gtfstools)
 library(dplyr)
 
-gtfs <- gtfstools::read_gtfs('../11 - GTFS/muni_pal/gtfs_files/gtfs_pal.zip')
-gtfs <- gtfstools::read_gtfs('../data-raw/gtfs/muni_con/2022/gtfs_con.zip')
-gtfs <- gtfstools::read_gtfs('../r5r/network/2022/muni_dou/gtfs_dourados_202212.zip')
+gtfs <- gtfstools::read_gtfs("../r5r/network/2022/gtfs_cit_202305_v01.zip")
+# gtfs <- gtfstools::read_gtfs('../data-raw/gtfs/muni_con/2022/gtfs_con.zip')
+# gtfs <- gtfstools::read_gtfs('../r5r/network/2022/muni_dou/gtfs_dourados_202212.zip')
+`%nin%` = Negate(`%in%`)
 
+linhas_teste <-  gtfs$routes$route_id[c(10,101)]
+
+linhas_remover <- gtfs$routes$route_id[c(81,84)]
+
+
+routes_teste <- gtfstools::filter_by_route_id(gtfs,route_id = linhas_teste)
+trips_teste <- routes_teste$trips$trip_id
+trips_teste <- gtfstools::filter_by_trip_id(routes_teste,trip_id = trips_teste)
+
+routes <- gtfs$routes %>% filter(route_id %nin% linhas_remover) %>% pull(route_id)
+routes <- routes[1:106]
+trips <- gtfs$trips$trip_id
+stops <- gtfs$stops$stop_id
+
+gtfs_filter <- gtfstools::filter_by_agency_id(gtfs,"1")
+gtfs_filter <- gtfstools::filter_by_route_id(gtfs_filter,route_id = routes)
+gtfs_filter <- gtfstools::filter_by_trip_id(gtfs_filter,trip_id = trips)
+gtfs_filter <- gtfstools::filter_by_stop_id(gtfs_filter,stop_id = stops) %>%
+  write_gtfs("../r5r/network/2022/muni_cit/gtfs_cit_202305_v01_filtrado.zip")
 
 
 a <- gtfs$routes$route_id
@@ -42,7 +62,7 @@ write_gtfs(teste, "../r5r/network/2022/muni_pal/gtfs_pal.zip")
 
 # FUNCAO PARA CONSTRUIR network -------------------------
 # graph.obj é salvo na pasta './otp/graphs/ano/cidade
-sigla_muni <- 'cit'
+sigla_muni <- 'man'
 ano <- 2022
 
 construir_graph_muni <- function(sigla_muni, ano) {
@@ -53,7 +73,7 @@ construir_graph_muni <- function(sigla_muni, ano) {
                 verbose = TRUE)
   
 }
-
+construir_graph_muni('man',2022)
 r5r::stop_r5()
 
 # aplicar funcao ------------------------------------------------------------------------------
